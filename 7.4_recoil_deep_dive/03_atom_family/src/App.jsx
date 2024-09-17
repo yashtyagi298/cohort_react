dfe7576388c68fdf3bc73a977599abc223146054
yashtyagi298 <tyagiyash097@gmail.com>
2024-09-17 21:08:22 +0530

    atom family done

---
+++ 7.4_recoil_deep_dive/03_atom_family/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {RecoilRoot , useRecoilState} from 'recoil'
+import { todosAtomFamily } from './atom';
+function App() {
+  return <RecoilRoot>
+    <Todo id={1}/>
+    <Todo id={2}/>
+
+  </RecoilRoot>
+}
+
+function Todo({id}){
+  const [todo,setTodo]=useRecoilState(todosAtomFamily(id));
+
+  return (
+    <>
+    {todo.title}
+    {todo.description}    
+    <br/>
+    </>
+  )
+}
+export default App
