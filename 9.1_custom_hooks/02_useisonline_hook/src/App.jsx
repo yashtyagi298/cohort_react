64925668b025d591d73a54748b9ac8051acdb721
yashtyagi298 <tyagiyash097@gmail.com>
2024-10-02 15:20:13 +0530

    useIsOnline hook

--- 9.1_custom_hooks/02_useisonline_hook/src/App.jsx
+++ 9.1_custom_hooks/02_useisonline_hook/src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { useEffect } from 'react';
+function useIsOnline(){
+  const [isOnline,setIsOnline] = useState(window.navigator.onLine);
+  useEffect(()=>{
+    window.addEventListener('online',()=>setIsOnline(true));
+    window.addEventListener('offline',()=>setIsOnline(false));
+  },[])
+  return isOnline;
+}
 function App() {
+ const isOnline = useIsOnline();
   return (
     <div>
-      
+      {isOnline ? "You are online yay ! " : "You are Offline❌"}
     </div>
   )
 }
